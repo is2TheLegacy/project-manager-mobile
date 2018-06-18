ee0ae203e5f334eba51d34041d6b883c5e3db199
Rafael Benegas <rafael.benegas@konecta.com.py>
2018-06-17 20:04:08 -0400

    Servicios para listar membresias en proyecto

---
+++ Backend/proyectos-alpha/src/main/java/py/fpuna/is2/proyectos/alpha/business/services/solicitudes/EstadosSolicitudColaboracion.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package py.fpuna.is2.proyectos.alpha.business.services.proyectos;
+
+/**
+ *
+ * @author rafae
+ */
+public final class EstadosProyecto {
+    public static String ACTIVO = "ACTIVO";
+    public static String INACTIVO = "INACTIVO";
+    public static String CULMINADO = "CULMINADO";
+    public static String EN_CURSO = "EN CURSO";
+    public static String PAUSADO = "PAUSADO";
+
+    private EstadosProyecto(){
+    }
+}
